360145a6ed8e46e9e0f04fe2998c7269e6114b2e
Alex Popov <alexpopovdm@outlook.com>
2024-09-21 18:25:07 +0300

    make build

---
+++ index.cd72217d.js
@@ -0,0 +1,2 @@
+function e(e,t){e.querySelectorAll("tr").forEach(function(e){var o=e.querySelectorAll("th, td"),r=o[t].cloneNode(!0);e.insertBefore(r,o[o.length-1])})}var t=document.querySelector("thead"),o=document.querySelector("tbody"),r=document.querySelector("tfoot");e(t,1),e(o,1),e(r,1);
+//# sourceMappingURL=index.cd72217d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const columnIndex = 1;
+function copyColumn(section, index) {
+    const rows = section.querySelectorAll("tr");
+    rows.forEach((row)=>{
+        const cells = row.querySelectorAll("th, td");
+        const targetCell = cells[index];
+        const clonedCell = targetCell.cloneNode(true);
+        row.insertBefore(clonedCell, cells[cells.length - 1]);
+    });
+}
+const thead = document.querySelector("thead");
+const tbody = document.querySelector("tbody");
+const tfoot = document.querySelector("tfoot");
+copyColumn(thead, columnIndex);
+copyColumn(tbody, columnIndex);
+copyColumn(tfoot, columnIndex);
+
+//# sourceMappingURL=index.f75de5e1.js.map
